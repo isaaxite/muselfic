01c8ca2d4275400af0dd4551a19970db04aa9c98
isaaxite <isaacgun@outlook.com>
2025-06-13 22:01:32 +0800

    feat: update data dir tree

---
+++ data/a64e3be5-3835-496f-b50b-29935d6ef5a2/metadata.sh
@@ -0,0 +1,12 @@
+#[required] album name
+METADATA_ALBUM="Suite bergamasque"
+#[required] number
+METADATA_TOTAL_TRACKS="4"
+#[required]
+METADATA_ALBUM_ARTIST="Claude Debussy"
+#[required] genre[,genre0][,genre1][,...]
+METADATA_GENRE="Classical"
+#[required] yyyy/mm/dd
+METADATA_RELEASE_DATA="1980"
+#[required] composer[,composer0][,composer1][,...]
+METADATA_COMPOSER="Claude Debussy"
