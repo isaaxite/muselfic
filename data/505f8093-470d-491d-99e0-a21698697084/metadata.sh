6db27d3edc446dca1323a2c17791d87289cae8ff
isaaxite <isaacgun@outlook.com>
2025-06-15 04:14:21 +0800

    feat: add album that Preludes, Book I

---
+++ data/505f8093-470d-491d-99e0-a21698697084/metadata.sh
@@ -0,0 +1,12 @@
+#[required] album name
+METADATA_ALBUM="Preludes, Book I"
+#[required] number
+METADATA_TOTAL_TRACKS="12"
+#[required]
+METADATA_ALBUM_ARTIST="Claude Debussy"
+#[required] genre[,genre0][,genre1][,...]
+METADATA_GENRE="classical"
+#[required] yyyy/mm/dd
+METADATA_RELEASE_DATA="1930"
+#[required] composer[,composer0][,composer1][,...]
+METADATA_COMPOSER="Claude Debussy"
