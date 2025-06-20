83153fdec077d8fb4ec82651263a62279fa3a824
isaaxite <isaacgun@outlook.com>
2025-06-21 05:17:48 +0800

    style: extract metadata logic into a sh file

--- muselfic.sh
+++ muselfic.sh
@@ -2,13 +2,14 @@
 . script/global.sh
 . script/util.sh
 . script/dist.sh
+. script/metadata.sh
 
 topest_avg1=$1
 shift
 case $topest_avg1 in
   ("dist") dist_data $@;;
   ("list") echo "list";;
-  ("metadata") echo "metadata";;
+  ("metadata") add_metadata_asset $@;;
   ("extract") echo "extract";;
   (*)
     echo "Wrong <rule>! You should type \"dist\" or \"list\" or \"metadata\" or \"extract\""
