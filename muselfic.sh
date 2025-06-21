21ff8e403bfd7098875fbe5e84c6673b8720b9e3
isaaxite <isaacgun@outlook.com>
2025-06-21 17:05:57 +0800

    style: extract extract logic into a sh file

--- muselfic.sh
+++ muselfic.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-. script/global.sh
-. script/util.sh
 . script/dist.sh
 . script/metadata.sh
+. script/extract.sh
 
 topest_avg1=$1
 shift
 case $topest_avg1 in
-  ("dist") dist_data $@;;
+  ("dist") dist_data "$@";;
   ("list") echo "list";;
-  ("metadata") add_metadata_asset $@;;
-  ("extract") echo "extract";;
+  ("metadata") add_metadata_asset "$@";;
+  ("extract") extract_audio_to "$@";;
   (*)
     echo "Wrong <rule>! You should type \"dist\" or \"list\" or \"metadata\" or \"extract\""
     exit 1
