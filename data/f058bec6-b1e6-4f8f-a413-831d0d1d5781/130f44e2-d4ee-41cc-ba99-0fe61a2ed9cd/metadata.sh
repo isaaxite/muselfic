6c021744b6d93a119d3e7cb80dc03390da4c7380
isaaxite <isaacgun@outlook.com>
2025-06-21 17:08:54 +0800

    feat: add 1 song

---
+++ data/f058bec6-b1e6-4f8f-a413-831d0d1d5781/130f44e2-d4ee-41cc-ba99-0fe61a2ed9cd/metadata.sh
@@ -0,0 +1,6 @@
+#[required] song name
+METADATA_TITLE="飘"
+#[required]
+METADATA_ARTIST="沙宝亮"
+#[required] number
+METADATA_TRACK_NUMBER="6"
---
+++ data/f058bec6-b1e6-4f8f-a413-831d0d1d5781/metadata.sh
@@ -0,0 +1,12 @@
+#[required] album name
+METADATA_ALBUM="男人好难"
+#[required] number
+METADATA_TOTAL_TRACKS="12"
+#[required]
+METADATA_ALBUM_ARTIST="沙宝亮"
+#[required] genre[,genre0][,genre1][,...]
+METADATA_GENRE="2013"
+#[required] yyyy/mm/dd
+METADATA_RELEASE_DATA=""
+#[required] composer[,composer0][,composer1][,...]
+METADATA_COMPOSER=""
